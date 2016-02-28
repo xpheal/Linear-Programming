0d5ef255c4ca1360c57040f6fc3057deaf6e0595
xpheal <xpheal@gmail.com>
2016-02-28 15:34:20 -0600

    h2p4

--- h2/p4/rental.jl
+++ h2/p4/rental.jl
@@ -18,23 +18,29 @@ agency_Coordinate = [
 	2 15
 ]
 
-distances = fill(0,10,10)
+# num = number of agencies
+(num,n) = size(agency_Coordinate)
+
+
+
+distances = fill(0,num,num)
 distances = convert(Array{Float64,2}, distances)
 
-for i in 1:10
-	for j in 1:10
+for i in 1:num
+	for j in 1:num
 		distances[i,j] = euclidean(agency_Coordinate[i,1],agency_Coordinate[i,2],agency_Coordinate[j,1], agency_Coordinate[j,2])
 	end
 end
 
-movements = fill(0,10,10)
-
 required_cars = [10;6;8;11;9;7;15;7;9;12]
 cars_present = [8;13;4;8;12;2;14;11;15;7]
 
-@defVar(m, movements[1:10,1:10] >= 0)
+@defVar(m, movements[1:num,1:num] >= 0)
 
-@addConstraint(m, flow[i in 1:10, j in 1:10], movements[i,j] + cars_present[i] - movements[j,i] >= required_cars[i])
+# println(distances)
+
+# @addConstraint(m, flow[i in 1:10, j in 1:10], movements[i,j] + cars_present[i] - movements[j,i] >= required_cars[i])
+@addConstraint(m, flow[i in 1:num], sum(movements[:,i]) + cars_present[i] - sum(movements[i,:]) >= required_cars[i])
 
 @setObjective(m, Min, vecdot(movements, distances))
 
@@ -43,3 +49,11 @@ status = solve(m)
 println("Total Cost: ", getObjectiveValue(m) * 0.5)
 
 println(getValue(movements))
+
+println(distances)
+
+println(required_cars)
+
+for i in 1:num
+	println(cars_present[i] + getValue(sum(movements[:,i])) - getValue(sum(movements[i,:])))
+end
